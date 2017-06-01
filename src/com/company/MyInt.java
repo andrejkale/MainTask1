0685a0dab2e3909cc84e655397f3c5b93d2fdd42
Andrej <andrejkaletest@gmail.com>
2017-06-01 13:23:29 +0300

    Task1Final

--- src/com/company/MyInt.java
+++ src/com/company/MyInt.java
@@ -12,20 +12,29 @@ public class MyInt {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         ArrayList<Integer> num = new ArrayList<Integer>();
+
         String number1;
+
 
         for (int i = 0; i < 6; i++) {
             num.add(Integer.parseInt(reader.readLine()));
-
         }
 
-        for (int i = 0; i < num.size(); i++) {
-            if (num.get(i) % 2 == 0) {
-                System.out.println("Even number " + " " + num.get(i));
-            } else {
-                System.out.println("Odd number " + " " + num.get(i));
-            }
-        }
+        evenOddNumbers(num); // even odds number print
+        minMaxNumbers(num); // min max numbers print
+
+        divideOn(num); // divede on 3,5,7,9
+
+        numbersWithThreediffdig(num); // number with three different digits print
+
+        happyNumbers(num); // happy numbers print
+
+        numbersHalfSum(num); //Find numbers of half sum of neighbors
+
+
+    }
+
+    private static void minMaxNumbers(ArrayList<Integer> num) {
         int max = num.get(0);
         int min = num.get(0);
         for (int i = 0; i < num.size(); i++) {
@@ -36,6 +45,45 @@ public class MyInt {
         }
         System.out.println("Max = " + max);
         System.out.println("Min = " + min);
+    }
+
+    private static void numbersHalfSum(ArrayList<Integer> num) {
+        String number1;
+        for (int i = 0; i < num.size() - 1; i++) {
+            number1 = num.get(i).toString();
+            if (number1.length() < 3){
+                break;
+            }
+            if (number1.charAt(i) == (number1.charAt(i) + number1.charAt(i + 1)) / 2){
+                System.out.println("Find numbers of half sum of neighbors " + num.get(i));
+            }
+
+        }
+    }
+
+    private static void happyNumbers(ArrayList<Integer> num) {
+        String number1;
+        for (int i = 0; i < num.size(); i++){
+            number1 = num.get(i).toString();
+            if (number1.length() == 4 && number1.charAt(0) + number1.charAt(1) == number1.charAt(2) + number1.charAt(3)){
+                System.out.println("Happy Numbers = " + num.get(i));
+            }
+
+        }
+    }
+
+    private static void numbersWithThreediffdig(ArrayList<Integer> num) {
+        String number1;
+        for (int i = 0; i < num.size(); i++) {
+            number1 = num.get(i).toString();
+            if (number1.length() == 3 && number1.charAt(0) != number1.charAt(1) && number1.charAt(1) != number1.charAt(2) && number1.charAt(0) != number1.charAt(2)) {
+                System.out.println("Numbers with three different digits " + num.get(i));
+            }
+
+        }
+    }
+
+    private static void divideOn(ArrayList<Integer> num) {
         for (int i = 0; i < num.size(); i++) {
             if (num.get(i) % 3 == 0) {
                 System.out.println("Divide on 3 " + num.get(i));
@@ -51,22 +99,16 @@ public class MyInt {
             }
 
         }
+    }
+
+    private static void evenOddNumbers(ArrayList<Integer> num) {
         for (int i = 0; i < num.size(); i++) {
-            number1 = num.get(i).toString();
-            if (number1.length() == 3 && number1.charAt(0) != number1.charAt(1) && number1.charAt(1) != number1.charAt(2) && number1.charAt(0) != number1.charAt(2)) {
-                System.out.println("Numbers with three different digits " + num.get(i));
+            if (num.get(i) % 2 == 0) {
+                System.out.println("Even number " + " " + num.get(i));
+            } else {
+                System.out.println("Odd number " + " " + num.get(i));
             }
-
         }
-        for (int i = 0; i < num.size(); i++){
-            number1 = num.get(i).toString();
-            if (number1.length() == 4 && number1.charAt(0) + number1.charAt(1) == number1.charAt(2) + number1.charAt(3)){
-                System.out.println("Happy Numbers = " + num.get(i));
-            }
-
-        }
-
-
     }
 }
 
--- src/com/company/Task1.java
+++ src/com/company/Task1.java
@@ -7,14 +7,14 @@ public class Task1 {
 
     public static void main(String[] args) {
         System.out.println("Hello !" + " " + Arrays.toString(args));
-        arrayInvert(args);
+        arrayInvert(args); // numbers invert print
         System.out.println(Arrays.toString(args));
 
 
 
     }
 
-    private static void arrayInvert(String[] args) {
+    private static void arrayInvert(String[] args) { // invert metod
         for (int k = 0; k < args.length / 2; k++) {
             String tmp = args[k];
             args[k] = args[args.length - k -1];
